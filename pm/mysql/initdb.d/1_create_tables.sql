9be5a9ac6fb5bf9ca83c6e6bec8fda54dd1db16f
kawaharakazuma <kawahara0525@icloud.com>
2021-10-25 12:45:20 +0900

    product-vision-tableを作成

--- pm/mysql/initdb.d/1_create_tables.sql
+++ pm/mysql/initdb.d/1_create_tables.sql
@@ -14,3 +14,35 @@ CREATE TABLE projects
     UNIQUE uq_key_name(group_id, key_name),
     UNIQUE uq_name(group_id, name)
 );
+
+CREATE TABLE product_visions
+(
+    id         CHAR(36)    NOT NULL,
+    title      VARCHAR(80) NOT NULL,
+    content    TEXT        NOT NULL,
+    author_id  CHAR(36)    NOT NULL,
+    editor_id  CHAR(36)    NOT NULL,
+    created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product_vision_comments
+(
+    id                CHAR(36) NOT NULL,
+    user_id           CHAR(36) NOT NULL,
+    content           TEXT     NOT NULL,
+    created_at        DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at        DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    product_vision_id CHAR(36) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_product_vision_id (product_vision_id)
+        REFERENCES product_visions (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE product_vision_comment_paths
+(
+    comment_parent_id CHAR(36) NOT NULL,
+    comment_child_id  CHAR(36) NOT NULL
+);
--- pm/mysql/initdb.d/2_insert_tables.sql
+++ pm/mysql/initdb.d/2_insert_tables.sql
@@ -9,3 +9,24 @@ values
     ("024d61d4-1d03-11ec-a478-0242ac180002", "022d74d6-1d03-11ec-a478-0242ac180002", "DEF", "デフォルトツール", "024d78d6-1d03-11ec-a478-0245ac180002", "024d78d6-1d03-11ec-a488-0242ac180002", default, default),
     ("024d81d3-1d03-11ec-a478-0242ac180002", "021d72d6-1d03-11ec-a478-0242ac180002", "PIED", "PiedPiper", "024d78d6-1d03-11ec-a478-0242ac380002", "024d78d6-1d03-11ec-a478-0842ac180002", default, default),
     ("024d31d2-1d03-11ec-a478-0242ac180002", "025d71d6-1d03-11ec-a478-0242ac180002", "NEO", "NeoPiedPiper", "024d78d6-1d03-11ec-a478-0246ac180002", "024d78d6-1d03-11ec-a498-0242ac180002", default, default);
+
+-- product_visions table test data
+INSERT INTO product_visions
+    (id, title, content, author_id, editor_id, created_at, updated_at)
+values
+    ("4495c574-34c2-4fb3-9ca4-3a7c79c267a6", "プロジェクト管理ツール", "テストです。", "4495c574-34c2-4fb3-9ca4-3a7c79c267a6", "9ca53205-47a9-4e6a-a6c1-65fc0a7ed1f1", default, default),
+    ("4487c574-34c2-4fb3-9ca4-3a7c79c267a6", "test", "test.", "4495c574-34c2-4fb3-9ca4-3a7c79c267a6", "9ca53205-47a9-4e6a-a6c1-65fc0a7ed1f1", default, default);
+
+-- product_vision_comments table test data
+INSERT INTO product_vision_comments
+    (id, user_id, content, created_at, updated_at, product_vision_id)
+values
+    ("40d891c2-f1fd-4291-a333-4a5815246442", "573418ec-f6d2-49e8-81f5-b6e641dbf00a", "コメントです！", default, default,"4495c574-34c2-4fb3-9ca4-3a7c79c267a6"),
+    ("40d811c2-f1fd-4291-a333-4a5815246442", "573418ec-f6d2-49e8-81f5-b6e641dbf00a", "test.", default, default,"4495c574-34c2-4fb3-9ca4-3a7c79c267a6");
+
+-- product_vision_comment_paths table test data
+INSERT INTO product_vision_comment_paths
+    (comment_parent_id, comment_child_id)
+values
+    ("cf77d797-597d-40d1-91ca-171cd4aff5b9", "d0278096-04f0-47ec-aeca-b15fe3886b46"),
+    ("cf77d797-597d-40d1-91ca-171cd4aff5b9", "d0278096-04f0-47ec-aeca-b15fe3886b46");
