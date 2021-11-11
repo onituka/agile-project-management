0677210e7e34627f974e48fd56382cfda8340c9c
Syuto Onituka <onituka.0701@gmail.com>
2021-11-11 16:34:59 +0900

    CORS middlewareを修正

--- pm/infrastructure/middleware/cors.go
+++ pm/infrastructure/middleware/cors.go
@@ -10,9 +10,10 @@ import (
 
 func CorsMiddlewareFunc() func(http.Handler) http.Handler {
 	corsWrapper := cors.New(cors.Options{
-		AllowedOrigins: config.Env.Cors.AllowedOrigins,
-		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
-		AllowedHeaders: []string{"Origin", "Content-Type", "Accept", "Accept-Language"},
+		AllowedOrigins:   config.Env.Cors.AllowedOrigins,
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Accept", "Accept-Language", "Authorization"},
+		AllowCredentials: true,
 	})
 
 	return corsWrapper.Handler
