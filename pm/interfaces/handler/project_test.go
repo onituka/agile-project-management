f9139266b3a5eb8f6457978af5781cd95f41dae4
Syuto Onituka <onituka.0701@gmail.com>
2021-11-18 00:20:20 +0900

    interface層のFetchProjectByIDテスト追加

--- pm/interfaces/handler/project_test.go
+++ pm/interfaces/handler/project_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
 
 	"github.com/onituka/agile-project-management/project-management/apperrors"
 	"github.com/onituka/agile-project-management/project-management/interfaces/mockusecase"
@@ -138,6 +139,148 @@ func Test_projectHandler_CreateProject(t *testing.T) {
 			w := httptest.NewRecorder()
 
 			h.CreateProject(w, r)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			testutil.AssertResponseBody(t, res, tt.fileSuffix)
+		})
+	}
+}
+
+func Test_projectHandler_FetchProjectByID(t *testing.T) {
+	type fields struct {
+		projectUsecase *mockusecase.MockProjectUsecase
+	}
+	tests := []struct {
+		name           string
+		fileSuffix     string
+		prepareMock    func(f *fields)
+		prepareRequest func(r *http.Request)
+	}{
+		{
+			name:       "200-正常",
+			fileSuffix: "200",
+			prepareMock: func(f *fields) {
+				ctx := mux.SetURLVars(&http.Request{}, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				}).Context()
+
+				in := &projectusecse.FetchProjectByIDInput{
+					ID: "024d71d6-1d03-11ec-a478-0242ac180002",
+				}
+
+				out := &projectusecse.FetchProjectByIDOutput{
+					ID:                "024d71d6-1d03-11ec-a478-0242ac180002",
+					GroupID:           "024d78d6-1d03-11ec-a478-0242ac180002",
+					KeyName:           "AAA",
+					Name:              "管理ツール1",
+					LeaderID:          "024d78d6-1d03-11ec-a478-0242ac184402",
+					DefaultAssigneeID: "024d78d6-1d03-11ec-a478-9242ac180002",
+					CreatedAt:         time.Date(2021, 11, 14, 0, 0, 0, 0, time.UTC),
+					UpdatedAt:         time.Date(2021, 11, 14, 0, 0, 0, 0, time.UTC),
+				}
+
+				f.projectUsecase.EXPECT().FetchProjectByID(ctx, in).Return(out, nil)
+			},
+			prepareRequest: func(r *http.Request) {
+				*r = *mux.SetURLVars(r, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				})
+			},
+		},
+		{
+			name:       "400-プロジェクトID不正",
+			fileSuffix: "400",
+			prepareMock: func(f *fields) {
+				ctx := mux.SetURLVars(&http.Request{}, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac1800023",
+				}).Context()
+
+				in := &projectusecse.FetchProjectByIDInput{
+					ID: "024d71d6-1d03-11ec-a478-0242ac1800023",
+				}
+
+				err := apperrors.InvalidParameter
+
+				f.projectUsecase.EXPECT().FetchProjectByID(ctx, in).Return(nil, err)
+			},
+			prepareRequest: func(r *http.Request) {
+				*r = *mux.SetURLVars(r, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac1800023",
+				})
+			},
+		},
+		{
+			name:       "404-IDが存在しない",
+			fileSuffix: "404",
+			prepareMock: func(f *fields) {
+				ctx := mux.SetURLVars(&http.Request{}, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				}).Context()
+
+				in := &projectusecse.FetchProjectByIDInput{
+					ID: "024d71d6-1d03-11ec-a478-0242ac180002",
+				}
+
+				err := apperrors.InvalidParameter
+
+				f.projectUsecase.EXPECT().FetchProjectByID(ctx, in).Return(nil, err)
+			},
+			prepareRequest: func(r *http.Request) {
+				*r = *mux.SetURLVars(r, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				})
+			},
+		},
+		{
+			name:       "500-DBエラー",
+			fileSuffix: "500",
+			prepareMock: func(f *fields) {
+				ctx := mux.SetURLVars(&http.Request{}, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				}).Context()
+
+				in := &projectusecse.FetchProjectByIDInput{
+					ID: "024d71d6-1d03-11ec-a478-0242ac180002",
+				}
+
+				err := apperrors.InternalServerError
+
+				f.projectUsecase.EXPECT().FetchProjectByID(ctx, in).Return(nil, err)
+			},
+			prepareRequest: func(r *http.Request) {
+				*r = *mux.SetURLVars(r, map[string]string{
+					"projectID": "024d71d6-1d03-11ec-a478-0242ac180002",
+				})
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gmctrl := gomock.NewController(t)
+
+			// gomockデータ生成
+			f := fields{
+				projectUsecase: mockusecase.NewMockProjectUsecase(gmctrl),
+			}
+
+			if tt.prepareMock != nil {
+				tt.prepareMock(&f)
+			}
+
+			// handler生成
+			h := NewProjectHandler(f.projectUsecase)
+
+			r := httptest.NewRequest(http.MethodGet, "/projects/024d71d6-1d03-11ec-a478-0242ac180002", nil)
+			w := httptest.NewRecorder()
+
+			if tt.prepareRequest != nil {
+				tt.prepareRequest(r)
+			}
+
+			h.FetchProjectByID(w, r)
 
 			res := w.Result()
 			defer res.Body.Close()
