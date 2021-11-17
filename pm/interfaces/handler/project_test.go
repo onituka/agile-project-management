996c248429e0da650890b2105a4f74d1ad6dc748
Yusuke Taira <ney07214545@icloud.com>
2021-11-17 22:46:51 +0900

    defer gmctrl.Finish()処理を削除

--- pm/interfaces/handler/project_test.go
+++ pm/interfaces/handler/project_test.go
@@ -120,7 +120,6 @@ func Test_projectHandler_CreateProject(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gmctrl := gomock.NewController(t)
-			defer gmctrl.Finish()
 
 			// gomockデータ生成
 			f := fields{
