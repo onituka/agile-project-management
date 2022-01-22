2ff2b7041daeaadf082c2e2fec077ab61a258849
Syuto Onituka <onituka.0701@gmail.com>
2022-01-23 01:08:42 +0900

    handler層のfetchTrashedProjectsテスト追加

--- pm/interfaces/handler/projecthandler/fetch_trashed_projects_handler_test.go
+++ pm/interfaces/handler/projecthandler/fetch_trashed_projects_handler_test.go
@@ -16,9 +16,9 @@ import (
 	"github.com/onituka/agile-project-management/project-management/usecase/projectusecase/projectoutput"
 )
 
-func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
+func TestFetchTrashedProjectsHandlerFetchTrashedProjects(t *testing.T) {
 	type fields struct {
-		searchProjectsUsecase *mockprojectusecase.MockSearchProjectsUsecase
+		fetchTrashedProjectsUsecase *mockprojectusecase.MockFetchTrashedProjectsUsecase
 	}
 
 	tests := []struct {
@@ -35,18 +35,17 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				}).Context()
 
-				in := &projectinput.SearchProjectsInput{
+				in := &projectinput.FetchTrashedProjectsInput{
 					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
 					Page:      1,
 					Limit:     2,
 				}
 
 				trashedAt := time.Date(2021, 11, 14, 0, 0, 0, 0, time.UTC)
 
-				out := &projectoutput.SearchProjectsOutput{
+				out := &projectoutput.FetchTrashedProjectsOutput{
 					TotalCount: 2,
-					Projects: []*projectoutput.SearchProjectOutput{
+					Projects: []*projectoutput.FetchTrashedProjectOutput{
 						{
 							ID:                "024d71d6-1d03-11ec-a478-0242ac180002",
 							ProductID:         "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
@@ -64,7 +63,7 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 							ProductID:         "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 							GroupID:           "024d78d6-1d03-11ec-a478-0242ac180002",
 							KeyName:           "BBB",
-							Name:              "管理ツールA",
+							Name:              "管理ツール2",
 							LeaderID:          "024d78d6-1d03-11ec-a478-0242ac184402",
 							DefaultAssigneeID: "024d78d6-1d03-11ec-a478-9242ac180002",
 							TrashedAt:         &trashedAt,
@@ -74,7 +73,7 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					},
 				}
 
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(out, nil)
+				f.fetchTrashedProjectsUsecase.EXPECT().FetchTrashedProjects(ctx, in).Return(out, nil)
 			},
 			prepareRequest: func(r *http.Request) {
 				*r = *mux.SetURLVars(r, map[string]string{
@@ -82,7 +81,6 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 				})
 
 				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "1")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
@@ -96,59 +94,26 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				}).Context()
 
-				in := &projectinput.SearchProjectsInput{
+				in := &projectinput.FetchTrashedProjectsInput{
 					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
 					Page:      1,
 					Limit:     2,
 				}
-				out := &projectoutput.SearchProjectsOutput{
+
+				out := &projectoutput.FetchTrashedProjectsOutput{
 					TotalCount: 0,
-					Projects:   make([]*projectoutput.SearchProjectOutput, 0),
+					Projects:   make([]*projectoutput.FetchTrashedProjectOutput, 0),
 				}
 
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(out, nil)
+				f.fetchTrashedProjectsUsecase.EXPECT().FetchTrashedProjects(ctx, in).Return(out, nil)
 			},
+
 			prepareRequest: func(r *http.Request) {
 				*r = *mux.SetURLVars(r, map[string]string{
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				})
 
 				q := r.URL.Query()
-				q.Set("keyword", "A")
-				q.Set("page", "1")
-				q.Set("limit", "2")
-				r.URL.RawQuery = q.Encode()
-			},
-		},
-		{
-			name:       "正常(検索にヒットしなかった場合)",
-			fileSuffix: "200-3",
-			prepareMock: func(f *fields) {
-				ctx := mux.SetURLVars(&http.Request{}, map[string]string{
-					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-				}).Context()
-
-				in := &projectinput.SearchProjectsInput{
-					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
-					Page:      1,
-					Limit:     2,
-				}
-				out := &projectoutput.SearchProjectsOutput{
-					TotalCount: 0,
-					Projects:   make([]*projectoutput.SearchProjectOutput, 0),
-				}
-
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(out, nil)
-			},
-			prepareRequest: func(r *http.Request) {
-				*r = *mux.SetURLVars(r, map[string]string{
-					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-				})
-
-				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "1")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
@@ -165,7 +130,6 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 				})
 
 				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "test")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
@@ -182,7 +146,6 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 				})
 
 				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "1")
 				q.Set("limit", "test")
 				r.URL.RawQuery = q.Encode()
@@ -196,16 +159,15 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				}).Context()
 
-				in := &projectinput.SearchProjectsInput{
+				in := &projectinput.FetchTrashedProjectsInput{
 					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
-					Page:      0,
+					Page:      1,
 					Limit:     2,
 				}
 
 				err := apperrors.InvalidParameter
 
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(nil, err)
+				f.fetchTrashedProjectsUsecase.EXPECT().FetchTrashedProjects(ctx, in).Return(nil, err)
 			},
 
 			prepareRequest: func(r *http.Request) {
@@ -213,8 +175,7 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				})
 				q := r.URL.Query()
-				q.Set("keyword", "A")
-				q.Set("page", "0")
+				q.Set("page", "1")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
 			},
@@ -227,16 +188,15 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				}).Context()
 
-				in := &projectinput.SearchProjectsInput{
+				in := &projectinput.FetchTrashedProjectsInput{
 					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
 					Page:      1,
 					Limit:     2,
 				}
 
 				err := apperrors.NotFound
 
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(nil, err)
+				f.fetchTrashedProjectsUsecase.EXPECT().FetchTrashedProjects(ctx, in).Return(nil, err)
 			},
 
 			prepareRequest: func(r *http.Request) {
@@ -244,7 +204,6 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				})
 				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "1")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
@@ -258,16 +217,15 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 					"productID": "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
 				}).Context()
 
-				in := &projectinput.SearchProjectsInput{
+				in := &projectinput.FetchTrashedProjectsInput{
 					ProductID: "4495c574-34c2-4fb3-9ca4-3a7c79c267a6",
-					KeyWord:   "A",
 					Page:      1,
 					Limit:     2,
 				}
 
 				err := apperrors.InternalServerError
 
-				f.searchProjectsUsecase.EXPECT().SearchProjects(ctx, in).Return(nil, err)
+				f.fetchTrashedProjectsUsecase.EXPECT().FetchTrashedProjects(ctx, in).Return(nil, err)
 			},
 
 			prepareRequest: func(r *http.Request) {
@@ -276,7 +234,6 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 				})
 
 				q := r.URL.Query()
-				q.Set("keyword", "A")
 				q.Set("page", "1")
 				q.Set("limit", "2")
 				r.URL.RawQuery = q.Encode()
@@ -288,23 +245,23 @@ func TestSearchProjectsHandlerSearchProjects(t *testing.T) {
 			gmctrl := gomock.NewController(t)
 
 			f := fields{
-				searchProjectsUsecase: mockprojectusecase.NewMockSearchProjectsUsecase(gmctrl),
+				fetchTrashedProjectsUsecase: mockprojectusecase.NewMockFetchTrashedProjectsUsecase(gmctrl),
 			}
 
 			if tt.prepareMock != nil {
 				tt.prepareMock(&f)
 			}
 
-			h := NewSearchProjectsHandler(f.searchProjectsUsecase)
+			h := NewFetchTrashedProjectsHandler(f.fetchTrashedProjectsUsecase)
 
-			r := httptest.NewRequest(http.MethodGet, "/products/{productID}/projects/search/", nil)
+			r := httptest.NewRequest(http.MethodGet, "/products/{productID}/projects/return-trash-box/", nil)
 			w := httptest.NewRecorder()
 
 			if tt.prepareRequest != nil {
 				tt.prepareRequest(r)
 			}
 
-			h.SearchProjects(w, r)
+			h.FetchTrashedProjects(w, r)
 
 			res := w.Result()
 			defer res.Body.Close()
